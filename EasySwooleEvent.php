c7c10e39ab3c2b329ee11cee2ee06e4779543666
ChrisSome <237227761@qq.com>
2020-12-14 11:18:10 +0800

    调试代码

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -49,8 +49,8 @@ class EasySwooleEvent implements Event
         $redisPoolConfig->setPort($redisConf['port']);
         $redisPoolConfig = \EasySwoole\RedisPool\Redis::getInstance()->register('redis',$redisPoolConfig);
         //配置连接池连接数
-        $redisPoolConfig->setMinObjectNum(20);
-        $redisPoolConfig->setMaxObjectNum(50);
+        $redisPoolConfig->setMinObjectNum(5);
+        $redisPoolConfig->setMaxObjectNum(20);
 
         //数据库
         $dbConf = Config::getInstance()->getConf('database')['MYSQL'];
@@ -65,9 +65,9 @@ class EasySwooleEvent implements Event
         $config->setGetObjectTimeout(3.0); //设置获取连接池对象超时时间
         $config->setIntervalCheckTime(30*1000); //设置检测连接存活执行回收和创建的周期
         $config->setMaxIdleTime(15); //连接池对象最大闲置时间(秒)
-        $config->setMinObjectNum(20); //设置最小连接池存在连接对象数量
 
-        $config->setMaxObjectNum(50); //设置最大连接池存在连接对象数量
+        $config->setMinObjectNum(5); //设置最小连接池存在连接对象数量
+        $config->setMaxObjectNum(20); //设置最大连接池存在连接对象数量
         $config->setAutoPing(5); //设置自动ping客户端链接的间隔
         DbManager::getInstance()->addConnection(new Connection($config));
     }
